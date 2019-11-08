0726117f75e8b9d62722aced11840df2fa5ce083
Thierry Girod <thierry.girod@hotmail.com>
2019-11-08 10:39:38 +0000

    Add createNewBlock method for adding blocks to the chain

--- dev/blockchain.ts
+++ dev/blockchain.ts
@@ -10,4 +10,32 @@ export class Blockchain {
         this.chain = [];
         this.newTransactions = [];
     }
+
+    createNewBlock(nonce: number,
+        previousBlockHash: string,
+        hash: string): Block {
+
+        const newBlock: Block = {
+            index: this.chain.length + 1,
+            timestamp: Date.now(),
+            transactions: this.newTransactions,
+            nonce: nonce,
+            hash: hash,
+            previousBlockHash: previousBlockHash
+        }
+
+        this.emptyNewTransactions();
+        this.addBlockToChain(newBlock)
+
+        return newBlock;
+    }
+
+    private addBlockToChain(block: Block): void {
+        this.chain.push(block);
+    }
+
+    private emptyNewTransactions(): void {
+        this.newTransactions = [];
+    }
+
 }
