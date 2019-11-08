11f69db6e8b693f7a3486914bc536f7da50c62f5
Thierry Girod <thierry.girod@hotmail.com>
2019-11-08 15:12:45 +0000

    Add /transaction endpoint

--- dev/api/api.ts
+++ dev/api/api.ts
@@ -1,11 +1,25 @@
 import { Blockchain } from "../blockchain";
 import express = require('express');
+import bodyParser = require('body-parser');
 
-const app =  express();
 
-const blockchain = new Blockchain();
-app.get('/blockchain', function (req, res) {
-    res.send(blockchain)
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+
+const bitcoin = new Blockchain();
+
+
+/**API Endpoints */
+app.get('/blockchain', function(req, res) {
+    res.send(bitcoin)
+});
+
+app.post('/transaction', function(req, res){
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    res.json({
+        note:`Transaction will be added in block ${blockIndex}.`
+    });
 });
 
 app.listen(3000, function() {
